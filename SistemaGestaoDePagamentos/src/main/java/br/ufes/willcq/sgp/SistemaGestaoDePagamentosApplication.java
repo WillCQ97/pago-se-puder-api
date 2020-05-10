6dda3efa4c6a2b0b41455a31246f66aa7a7b143c
WillCQ97 <willian.cqueiroz@gmail.com>
2020-05-10 18:22:57 -0300

    Backend: dependencias maven e configuração do banco de dados

---
+++ SistemaGestaoDePagamentos/src/main/java/br/ufes/willcq/sgp/SistemaGestaoDePagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.ufes.willcq.sgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoDePagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoDePagamentosApplication.class, args);
+	}
+
+}
---
+++ SistemaGestaoDePagamentos/src/test/java/br/ufes/willcq/sgp/SistemaGestaoDePagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.willcq.sgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoDePagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
