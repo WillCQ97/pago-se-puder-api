95d33d55d0b0c6b18fc2066b34cd93cdc735e1ce
WillCQ97 <willian.cqueiroz@gmail.com>
2020-07-05 12:15:26 -0300

    leves alterações

--- SistemaGestaoDePagamentos/src/main/java/br/ufes/willcq/sgp/model/Pagamento.java
+++ SistemaGestaoDePagamentos/src/main/java/br/ufes/willcq/sgp/model/Pagamento.java
@@ -1,7 +1,5 @@
 package br.ufes.willcq.sgp.model;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 import javax.persistence.Basic;
@@ -9,13 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 /* Supostamente deverá ser criado uma classe PagamentoServiceImpl, e interfaces para 
  * PagamentoService e PagamentoRepository, como foi feito para Produto */
